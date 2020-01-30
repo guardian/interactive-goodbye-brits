e436a235a93cfb0257d469ea500c6ee756d33dca
blongden73 <hello@benlongdendesign.co.uk>
2020-01-30 12:36:12 +0000

    js-magic

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,50 @@
-// change language on loop
-// const languages = [
-//   'Hej då',
-//   'Salut',
-//   'Arrivederci',
-//   'Dovijdane',
-//   'Güle güle',
-//   'Ardievas',
-//   'Do videnia',
-//   'La revedere',
-//   'Tschüss',
-//   'Adiós',
-//   'Zbogom',
-//   'Äddi',
-//   'Auf wiedersehen',
-//   'Doviđenja',
-//   'Näkemiin',
-//   'Aντιο σας',
-//   'Viso gero',
-//   'Do widzenia',
-//   'Viszontlátásra',
-//   'Adeus',
-//   'Head aega',
-//   'Au revoir',
-//   'Vaarwel',
-//   'Addiju',
-//   'Slán',
-//   'Farvel',
-//   'Goodbye'
-// ];
+function languages() {
+const languages = [
+  '🇸🇪 Hej då',
+  '🇫🇷 Salut',
+  'Arrivederci',
+  'Dovijdane',
+  'Güle güle',
+  'Ardievas',
+  'Do videnia',
+  'La revedere',
+  'Tschüss',
+  'Adiós',
+  'Zbogom',
+  'Äddi',
+  'Auf wiedersehen',
+  'Doviđenja',
+  'Näkemiin',
+  'Aντιο σας',
+  'Viso gero',
+  'Do widzenia',
+  'Viszontlátásra',
+  'Adeus',
+  'Head aega',
+  'Au revoir',
+  'Vaarwel',
+  'Addiju',
+  'Slán',
+  'Farvel',
+  '🇪🇺 Goodbye'
+];
 
-// // 'Slán agus beannacht'
-
-// let i = 0;
-// const loopLanguages = setInterval(() => {
-//   document.getElementById('change-lang').innerHTML = languages[i];
-//   if (i == (languages.length - 1)) {
-//     i = 0;
-//   } else {
-//     i++;
-//   }
-// }, 1500)
+// 'Slán agus beannacht'
+let i = 0;
+const loopLanguages = setInterval(() => {
+  document.getElementById('change-lang').innerHTML = languages[i];
+  if (i == (languages.length - 1)) {
+    i = 0;
+  } else {
+    i++;
+  }
+}, 1500)
+}
 
 function readMore(){
   var readMore = document.querySelectorAll('.bye-brits__content__readmore');
   var content = document.querySelectorAll('.bye-brits__content');
-  for(i = 0; i < readMore.length; i++){
+  for(var i = 0; i < readMore.length; i++){
     readMore[i].addEventListener('click', function(){
      console.log(this);
      var parent = this.parentNode;
@@ -59,7 +59,23 @@ function readMore(){
   }
 }
 
+function scroll() {
+  var goodbye = document.querySelector('.bye-brits__header__goodbye-brits');
+  var goodbyeTrack = document.querySelector('.headline_tracker');
+  var stickme = document.querySelector('.stickme');
+  document.addEventListener('scroll', function(){
+    var positionGoodbye = goodbyeTrack.getBoundingClientRect();
+    if(positionGoodbye.top <= -100) {
+      stickme.classList.add('showme');
+    } else {
+      stickme.classList.remove('showme');
+    }
+  });
+}
+
 function init(){
   readMore();
+  scroll();
+  languages();
 }
 init();
